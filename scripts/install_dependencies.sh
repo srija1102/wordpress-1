74f07351a6a08de4be91ed375781f69b060ed7d0
revanthsariga12 <120722376+revanthsariga12@users.noreply.github.com>
2023-01-24 13:49:57 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-if [ -f /etc/init.d/mysql* ]; then
-    echo "Mysql已经安装!"
-else
-	yum install -y mysql-server	
-	service mysqld start
-	mysqladmin -u root password 'fit2cloud'
-	mysql -u root -pfit2cloud -e "create database wordpress"
-	service mysqld stop
-	echo "Mysql安装成功!"
-fi
-
-if [ -f /etc/init.d/httpd* ]; then
-    echo "Apache已经安装!"
-else
-	yum groupinstall -y "Web Server" "PHP Support"
-	yum install -y php-mysql
-	echo "Apache安装成功!"
-fi
+sudo amazon-linux-extras install php7.4
+sudo yum install -y httpd mariadb-server php
