57d223ce402ef4ee05f7c06c91da824e1d434a31
srija1102 <120572388+srija1102@users.noreply.github.com>
2023-02-21 17:54:48 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo service docker start
 sudo docker build /var/www/html/WordPress
-#sudo docker-compose -f docker-compose.yml up -d
-sudo /usr/local/bin/docker-compose -f /home/ec2-user/app/docker-compose.yaml up -d
+sudo docker-compose up -d
+
